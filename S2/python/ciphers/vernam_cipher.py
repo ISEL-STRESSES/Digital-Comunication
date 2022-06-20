69775cf3b756776b54c8bea2496347b998ec7df2
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-20 16:50:09 +0100

    vernam cipher kinda working with problems when writing to file due to exceeding valid char code To try to fix via circular

--- S2/python/ciphers/vernam_cipher.py
+++ S2/python/ciphers/vernam_cipher.py
@@ -1,25 +1,42 @@
 import random
 import string
 
-from Python.string_fountain import string_fountain_gen
 
 def vernam_cipher(file):
     file = open(file, "r")
     plain_text = file.readlines()
     key_generated = []
     keycnt = 0
-    f = open("vernam_ciphered.txt", "w")
+    f = open("vernam_ciphered.txt", "w", encoding="utf-8")
+    cnt = 0
     for lines in plain_text:
-        ciphered = ""
-        for n in lines:
-            key = random.randint(0, 255)
-            ciphered += chr(ord(n)-key)
-            key_generated[keycnt] = key
-            keycnt+=1
-
-        f.write(ciphered)
+        cipher, key = makeVernamCypher(lines)
+        print(cipher)
+        f.write(cipher)
+        key_generated[cnt] = key
+        cnt += 1
+    #for lines in plain_text:
+    #    ciphered = ""
+    #    for n in lines:
+    #        key = random.randint(0, 255)
+    #        ciphered += chr(ord(n)-key)
+    #        key_generated[keycnt] = key
+    #        keycnt+=1
+    #    f.write(ciphered)
     f.close()
     return
+
+
+def makeVernamCypher(text):
+    key = key_gen(len(text))
+    ciphered = ""
+    p = 0
+    for char in text:
+        ciphered += chr(ord(char) ^ key[p])
+        p += 1
+        if p == len(key):
+            p = 0
+    return ciphered, key
 
 
 def key_gen(numb):
