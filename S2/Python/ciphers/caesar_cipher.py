2d504d12edc527c87acbbb097bdd5d48a39559c2
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-23 12:59:31 +0100

    caesar completely DONE

--- S2/Python/ciphers/caesar_cipher.py
+++ S2/Python/ciphers/caesar_cipher.py
@@ -5,36 +5,32 @@ caesar_files_output_path = "caesar_output/"
 
 
 def caesar_cipher(file):
-    # read plain text
-    plain_text_file = open(test_files_path + file, "rb")
-    plain_text = plain_text_file.read()
-    plain_text_file.close()
-
-    # cipher text and write to file
+    # get file name and extension
     file_name = file.partition('.')[0]
     get_ext = file.partition('.')[2]
-    ciphered_file = open(caesar_files_output_path + file_name + "_caesar_ciphered." + get_ext, "wb")
-    ciphered = make_caesar_cypher(plain_text)
-    ciphered_file.write(bytearray(ciphered))
-    ciphered_file.close()
 
-
-def caesar_cipher_img(file):
     try:
-        img = Image.open(test_files_path + file)
-        pixel_map = img.load()
-        im = Image.new('L', img.size)
-        pixels_new = im.load()
+        # open image
+        plain_img = Image.open(test_files_path + file)
 
-        file_name = file.partition('.')[0]
-        get_ext = file.partition('.')[2]
+        # cipher image
+        ciphered_img = make_caesar_cypher_img(plain_img)
 
-        for i in range(img.size[0]):
-            for j in range(img.size[1]):
-                pixels_new[i, j] = (pixel_map[i, j] + 100) % 256
-        im.save(caesar_files_output_path + file_name + "_caesar_ciphered." + get_ext)
+        # save ciphered image
+        ciphered_img.save(caesar_files_output_path + file_name + "_caesar_ciphered." + get_ext)
     except IOError:
-        print("error")
+        # read plain text
+        plain_file = open(test_files_path + file, "rb")
+        plain_text = plain_file.read()
+        plain_file.close()
+
+        # cipher text
+        ciphered = make_caesar_cypher(plain_text)
+
+        # write cipher to file
+        ciphered_file = open(caesar_files_output_path + file_name + "_caesar_ciphered." + get_ext, "wb")
+        ciphered_file.write(bytearray(ciphered))
+        ciphered_file.close()
 
 
 def make_caesar_cypher(text):
@@ -42,6 +38,16 @@ def make_caesar_cypher(text):
     numb = 3
     for n in range(len(text)):
         ciphered.append((text[n] + numb))
+    return ciphered
+
+
+def make_caesar_cypher_img(img):
+    pixel_map = img.load()
+    ciphered = Image.new('L', img.size)
+    pixels_new = ciphered.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            pixels_new[i, j] = (pixel_map[i, j] + 3) % 256
     return ciphered
 
 
@@ -53,18 +59,42 @@ def make_caesar_decipher(text):
     return ciphered
 
 
-def caesar_decipher(file):
-    # read ciphered text
-    deciphered_text_file = open(caesar_files_output_path + file, "rb")
-    ciphered_text = deciphered_text_file.read()
+def make_caesar_decipher_img(img):
+    pixel_map = img.load()
+    im = Image.new('L', img.size)
+    pixels_new = im.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            pixels_new[i, j] = (pixel_map[i, j] - 3) % 256
+    return im
 
-    # decipher text and write to file
+
+def caesar_decipher(file):
+    # get file name and extension
     file_name = file.partition("_caesar_ciphered")[0]
     get_ext = file.partition('.')[2]
-    f = open(caesar_files_output_path + file_name + "_caesar_deciphered." + get_ext, "wb")
-    deciphered = make_caesar_decipher(ciphered_text)
-    f.write(bytearray(deciphered))
-    f.close()
+
+    try:
+        # open image
+        img = Image.open(caesar_files_output_path + file)
+
+        # cipher image
+        im = make_caesar_decipher_img(img)
+
+        # save ciphered image
+        im.save(caesar_files_output_path + file_name + "_caesar_deciphered." + get_ext)
+    except IOError:
+        # read ciphered text
+        deciphered_text_file = open(caesar_files_output_path + file, "rb")
+        ciphered_text = deciphered_text_file.read()
+
+        # decipher text
+        deciphered = make_caesar_decipher(ciphered_text)
+
+        # write deciphered to file
+        f = open(caesar_files_output_path + file_name + "_caesar_deciphered." + get_ext, "wb")
+        f.write(bytearray(deciphered))
+        f.close()
 
 
 def cipher_test_files():
@@ -80,12 +110,11 @@ def decipher_test_files():
     caesar_decipher("a_caesar_ciphered.txt")
     caesar_decipher("alice29_caesar_ciphered.txt")
     caesar_decipher("cp_caesar_ciphered.htm")
-    caesar_decipher("lena_caesar_deciphered.bmp")
+    caesar_decipher("lena_caesar_ciphered.bmp")
     caesar_decipher("Person_caesar_ciphered.java")
     caesar_decipher("progc_caesar_ciphered.c")
 
 
 if __name__ == '__main__':
-    #cipher_test_files()
-    #decipher_test_files()
-    caesar_cipher_img("lena.bmp")
+    cipher_test_files()
+    decipher_test_files()
