dfb9c6cb18470c90fe6c2c4cf70538fadb722add
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-23 11:32:19 +0100

    arduino done I think

--- S2/Python/arduino/arduino.py
+++ S2/Python/arduino/arduino.py
@@ -5,10 +5,13 @@ from serial import Serial
 def read_data_from_arduino():
     #configuration for acessing arduino COM3 9600
     portName = "COM3"
-    ser = serial.Serial(portName, 9600)
+    ser = serial.Serial(portName, 9600, timeout=2)
     #ser.flush()
-    data = ser.readline(1000)
-    print(data)
+    #ser.read_until()
+    #for i in range(1,13):
+    #    data = ser.readline()
+    #    print(data)
+    data = ser.readline()
     f = open("arduino_read.txt", "wb")
     f.write(data)
     f.close()
