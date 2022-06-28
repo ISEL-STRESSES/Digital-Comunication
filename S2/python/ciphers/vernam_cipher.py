f0b5fa2a3e1caa59f30ca66adbf7d2ee9855d51b
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-28 20:27:20 +0100

    ciphers done

--- S2/python/ciphers/vernam_cipher.py
+++ S2/python/ciphers/vernam_cipher.py
@@ -1,29 +1,46 @@
 import secrets
+from PIL import Image
 test_files_path = "../CD_TestFiles/"
-caesar_files_output_path = "vernam_output/"
+vernam_files_output_path = "vernam_output/"
 
 
 def vernam_cipher(file, key_file):
-    # read plain text
-    plain_text_file = open(test_files_path + file, "rb")
-    plain_text = plain_text_file.read()
-    plain_text_file.close()
-
-    # generate key
-    key = secrets.token_bytes(len(plain_text) + secrets.randbelow(5))
-
-    # write key
+    # get file name and extension
     file_name = file.partition('.')[0]
-    get_ext = key_file.partition('.')[2]
-    key_file = open(caesar_files_output_path + key_file, "wb")
+    get_ext = file.partition('.')[2]
+
+    try:
+        # open image
+        plain_img = Image.open(test_files_path + file)
+
+        # generate key
+        width, height = plain_img.size
+        key = secrets.token_bytes(width*height + secrets.randbelow(width*height//5))    # integer division
+
+        # cipher image
+        ciphered_img = make_vernam_cypher_img(plain_img, key)
+
+        # save ciphered image
+        ciphered_img.save(vernam_files_output_path + file_name + "_vernam_ciphered." + get_ext)
+    except IOError:
+        # read plain text
+        plain_text_file = open(test_files_path + file, "rb")
+        plain_text = plain_text_file.read()
+        plain_text_file.close()
+
+        # generate key
+        key = secrets.token_bytes(len(plain_text) + secrets.randbelow(5))
+
+        # cipher text and write to file
+        f = open(vernam_files_output_path + file_name + "_vernam_ciphered"+"."+get_ext, "wb")
+        cipher = make_vernam_cypher(plain_text, key)
+        f.write(bytearray(cipher))
+        f.close()
+
+    # write key to file
+    key_file = open(vernam_files_output_path + key_file, "wb")
     key_file.write(key)
     key_file.close()
-
-    # cipher text and write to file
-    f = open(caesar_files_output_path + file_name + "_vernam_ciphered"+"."+get_ext, "wb")
-    cipher = make_vernam_cypher(plain_text, key)
-    f.write(bytearray(cipher))
-    f.close()
 
 
 def make_vernam_cypher(text, key):
@@ -37,24 +54,50 @@ def make_vernam_cypher(text, key):
     return ciphered
 
 
+def make_vernam_cypher_img(img, key):   # ciphers and deciphers image
+    pixel_map = img.load()
+    ciphered = Image.new('L', img.size)
+    pixels_new = ciphered.load()
+    p = 0
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            pixels_new[i, j] = (pixel_map[i, j] ^ key[p]) % 256
+            p += 1
+            if p == len(key):
+                p = 0
+    return ciphered
+
+
 def vernam_decipher(file, key_file):
-    # read ciphered text
-    ciphered_text_file = open(caesar_files_output_path + file, "rb")
-    cypher_text = ciphered_text_file.read()
-    ciphered_text_file.close()
+    # get file name and extension
+    file_name = key_file.partition(vernam_files_output_path + "_vernam_key")[0]
+    get_ext = file.partition('.')[2]
 
     # read key
-    file_name = key_file.partition(caesar_files_output_path + "_vernam_key")[0]
-    get_ext = key_file.partition('.')[2]
-    file_key = open(caesar_files_output_path + key_file, "rb")
+    file_key = open(vernam_files_output_path + key_file, "rb")
     key = file_key.read()
     file_key.close()
 
-    # decipher text and write to file
-    f = open(caesar_files_output_path + "vernam_deciphered_"+file_name+"."+get_ext, "wb")
-    deciphered = make_vernam_cypher(cypher_text, key)
-    f.write(bytearray(deciphered))
-    f.close()
+    try:
+        # open image
+        img = Image.open(vernam_files_output_path + file)
+
+        # cipher image
+        im = make_vernam_cypher_img(img, key)
+
+        # save ciphered image
+        im.save(vernam_files_output_path + file_name + "_vernam_deciphered." + get_ext)
+    except IOError:
+        # read ciphered text
+        ciphered_text_file = open(vernam_files_output_path + file, "rb")
+        cypher_text = ciphered_text_file.read()
+        ciphered_text_file.close()
+
+        # decipher text and write to file
+        f = open(vernam_files_output_path + file_name + "_vernam_deciphered."+get_ext, "wb")
+        deciphered = make_vernam_cypher(cypher_text, key)
+        f.write(bytearray(deciphered))
+        f.close()
 
 
 def cipher_test_files():
