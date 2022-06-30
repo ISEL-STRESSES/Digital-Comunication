59e31f1fb5b8242ab6e59dd9b9e07dd9a6d02fa9
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-30 20:09:37 +0100

    nrzu decoder DONE added noise do encoder encoder noise is not saved to file

--- S2/Python/SCD/emitter.py
+++ S2/Python/SCD/emitter.py
@@ -1,3 +1,5 @@
+import random
+
 from bitstring import BitArray
 from matplotlib import pyplot as plt
 import numpy as np
@@ -13,21 +15,27 @@ def nrzu_store(vector, file):
     # get file name and extension
     file_name = file.partition('.')[0]
     get_ext = file.partition('.')[2]
-    nrzu_file_name = file_name + "_NRZU." + get_ext
+    nrzu_file_name = file_name + "_nrzu." + get_ext
 
     file = open(scd_output_path + nrzu_file_name, "wb")
     file.write(bytearray(vector))
     file.close()
 
 
-def nrzu_coder(data, amp, bit_rate):
+def find_value():
+    return
+    # read dile until a value is find for frequency
+    # find by searching for something diferent thn 0 and until the next first value is equal
+
+
+def nrzu_coder(data, amp, bit_rate, noise = 0):
     time = np.arange(ORIGIN, bit_rate * len(data), bit_rate / SAMPLES_PER_BIT)
     dots = []
-
+    val = find_value()
     for bit in data:
         aux = 0
         for i in range(aux, aux + SAMPLES_PER_BIT):
-            dots.append(bit * amp)
+            dots.append(bit * amp + 0.1 * random.randint(0,noise))
         aux += SAMPLES_PER_BIT
 
     plt.plot(time, dots)
@@ -47,7 +55,7 @@ def file_load(file_name):
 
 
 if __name__ == '__main__':
-    a = file_load(test_files_path + "a.txt")
-    # print(a)
-    vec = nrzu_coder(data=a, amp=5, bit_rate=0.1)
-    nrzu_store(vec, "a.txt")
+    #a = file_load(test_files_path + "a.txt")
+    a = file_load(test_files_path + "test_scd.txt")
+    vec = nrzu_coder(data=a, amp=5, bit_rate=0.1, noise=5)
+    nrzu_store(vec, "t.txt")
--- S2/Python/SCD/receiver.py
+++ S2/Python/SCD/receiver.py
@@ -1,9 +1,73 @@
+import numpy as np
+from bitstring import BitArray
+from matplotlib import pyplot as plt
 
-def NRZU_Decoder(data):
+test_files_path = "../CD_TestFiles/"
+scd_output_path = "scd_output/"
+
+SAMPLES_PER_BIT = 10
+ORIGIN = 0
 
 
-def PSK_Demodulator(data):
+def find_min_bit_time(data):
+    last = 0
+    cnt = 1
+    min = len(data)
+    for n in range(len(data)):
+        if last == data[n]:
+            cnt += 1
+        else:
+            last = data[n]
+            if cnt >1 and cnt < min:
+                min = cnt
+            cnt = 1
+    return min
 
+
+def find_freq(data):
+    last = 0
+    cnt = 0
+    for n in range(len(data)):
+        if last!=0:
+            if data[n] == last:
+                break
+            else:
+                if data[n] != 0:
+                    last = data[n]
+        last = data[n]
+    return cnt
+
+
+def nrzu_decoder(data):
+    size = len(data)
+    print(size)
+    print(find_min_bit_time(data))
+    cnt = 1
+    decoded = []
+
+    for byte in data:
+        if cnt == SAMPLES_PER_BIT:
+            cnt = 1
+            if byte > 0:
+                decoded.append(1)
+            else:
+                decoded.append(0)
+        cnt += 1
+    return decoded
+
+#def PSK_Demodulator(data):
+
+
+def file_load(file_name):
+    file = open(file_name, "rb")
+    txt = file.read()
+    file.close()
+    #text_as_bit = BitArray(bytes=txt)
+    return txt
 
 
 if __name__ == '__main__':
+    a = file_load(scd_output_path + "t_nrzu.txt")
+    vec = nrzu_decoder(data=a)
+    print(vec)
+    text_as_bit = BitArray(vec)
