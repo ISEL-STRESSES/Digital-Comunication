cefaa047d40fbb485b51b9636b32829554e70de5
Tiago Pardal <tiagopardl@hotmail.com>
2022-06-30 17:58:35 +0100

    nrzu encoder DONE

--- S2/Python/SCD/emitter.py
+++ S2/Python/SCD/emitter.py
@@ -2,27 +2,21 @@ from bitstring import BitArray
 from matplotlib import pyplot as plt
 import numpy as np
 
-local_path = "../CD_TestFiles/"
-test_path = "./Test_Output/"
+test_files_path = "../CD_TestFiles/"
+scd_output_path = "scd_output/"
 
 SAMPLES_PER_BIT = 10
 ORIGIN = 0
 
 
-def nrzu_store_line(vector, file_name, opened, file=None):
-    if not opened:
-        file = open(file_name, "wb")
-    file.write(vector)
-    file.close()
+def nrzu_store(vector, file):
+    # get file name and extension
+    file_name = file.partition('.')[0]
+    get_ext = file.partition('.')[2]
+    nrzu_file_name = file_name + "_NRZU." + get_ext
 
-
-def nrzu_store(vectors, file_name):
-    dest = test_path + file_name.partition(".")[0] + "_NRZU." + file_name.partition(".")[2]
-    if len(vectors) == 1:
-        nrzu_store_line(vectors, dest, False)
-    file = open(dest, "w")
-    for vector in vectors:
-        file.write(vector)
+    file = open(scd_output_path + nrzu_file_name, "wb")
+    file.write(bytearray(vector))
     file.close()
 
 
@@ -45,25 +39,15 @@ def nrzu_coder(data, amp, bit_rate):
 
 
 def file_load(file_name):
-    file = open(file_name, "r").readlines() # open(file_name, "rb").readlines()
-    print(file)
-    print()
-    byte = BitArray()
-    for line in file:
-        # transformar uma linha em um array de chars nao ha a puta do .map{}
-        # line.map{word -> word.map{it}}.map{ it.toBin() } :list<list<char>>
-        for word in line:
-            for char in word:
-                byte.append(bytes(char))
-        print(bytes(line))
-        print()
-
-    bits = [bin(bit)[2:] for bit in bytes(byte)]
-    return bits
+    file = open(file_name, "rb")
+    txt = file.read()
+    file.close()
+    text_as_bit = BitArray(bytes = txt)
+    return text_as_bit
 
 
 if __name__ == '__main__':
-    a = file_load(local_path + "a.txt")
-    print(a)
-    # vec = nrzu_coder(data=a, amp=5, bit_rate=0.1)
-    # nrzu_store(vec, "a.txt")
+    a = file_load(test_files_path + "a.txt")
+    # print(a)
+    vec = nrzu_coder(data=a, amp=5, bit_rate=0.1)
+    nrzu_store(vec, "a.txt")
